0d604708ceb67865b079bc72da4e15e6f9957ef6
Jovi <ijovi23@gmail.com>
2015-09-05 21:49:39 +0800

    v1.0

---
+++ JovisConfirmView/JovisConfirmView/JovisConfirmView.h
@@ -0,0 +1,13 @@
+//
+//  JovisConfirmView.h
+//  JovisConfirmView
+//
+//  Created by Jovi on 15/9/5.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JovisConfirmView : NSObject
+
+@end
---
+++ JovisConfirmView/JovisConfirmView/JovisConfirmView.m
@@ -0,0 +1,13 @@
+//
+//  JovisConfirmView.m
+//  JovisConfirmView
+//
+//  Created by Jovi on 15/9/5.
+//
+//
+
+#import "JovisConfirmView.h"
+
+@implementation JovisConfirmView
+
+@end
